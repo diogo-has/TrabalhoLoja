3da88308c740aa2038560a66d43bcbc3e1ee367a
diogo-has <dioglois.games@gmail.com>
2024-03-05 17:54:57 -0300

    pedido2

--- Order/script.js
+++ Order/script.js
@@ -1,5 +1,20 @@
-var opt = document.getElementById("order").innerHTML
-var opts = document.getElementById("orders")
+var counter = 1;
 
-opts.innerHTML += opt;
-alert('hi')
+function test() {
+    if (counter >= 15) {
+        alert("Máximo de 15 itens por pedido.\nCaso deseje pedir mais, favor fazer múltiplos pedidos.");
+        return;
+    };
+
+    counter++;
+    var optHTML = document.getElementById("order1").innerHTML;
+    var opts = document.getElementById("orders");
+
+    var newOpt = document.createElement('div');
+    newOpt.classList.add('order');
+    newOpt.id = 'order' + counter;
+    newOpt.innerHTML = optHTML;
+
+    opts.appendChild(newOpt);
+}
+
